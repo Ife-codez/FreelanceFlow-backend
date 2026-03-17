0f1643a3024b3fcab8b5e1e8dd145619ff3bfd39
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2026-03-17 01:31:57 -0700

    .

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     HttpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? "none" : "lax",
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 1000 * 60 * 60 * 24 * 7
   })
   return token
