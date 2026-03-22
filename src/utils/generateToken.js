4ce22906b5c4f808f2925330e57386159ea3d03a
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2026-03-22 13:28:19 -0700

    fixed a typo issue with token generation

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -6,7 +6,7 @@ export const generateToken = (userId, res) => {
 
 
   res.cookie("jwt", token, {
-    HttpOnly: true,
+    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 1000 * 60 * 60 * 24 * 7
